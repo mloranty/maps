4f1f27809001a7d54369096bf100d228f1c0c119
Mike Loranty <mloranty@colgate.edu>
2020-04-15 13:55:11 -0400

    improved map code added script for map of FLARE 2019 field sites, including pulling base maps from the web

---
+++ flare_uas_2019_map.R
@@ -0,0 +1,66 @@
+######################
+# script to create map of Siberia sites 
+# sampled in 2019 for FLARE project
+#
+# ML 04/15/20
+######################
+
+rm(list=ls())
+
+# load packages
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(DT)
+library(sf)
+library(ggmap) # devtools::install_github("dkahle/ggmap")
+library(ggrepel)
+library(raster)
+library(rgdal)
+library(rasterVis)
+library(RColorBrewer)
+library(cowplot)
+library(ggspatial)
+library(maps)
+library(maptools)
+library(ggsn)
+
+register_google(key = "AIzaSyCmmdl7DjKfreahP00kPJ-ybRz6FeeXsDw")
+t <- get_map('texas', zoom = 6, maptype = 'satellite')
+qmap(location =  , zoom = 14, maptype = "satellite")
+
+# get a background map based roughly on the bounding box for the sampling points
+# this should be giving google satellite imagery, but it is not...
+c <- get_map(c(left = 160.2, bottom = 67.7, right = 162.7, top = 68.9),
+             maptype = "satellite", zoom = 10, source = "google", force = T)
+
+# was thinking about using Logan's biomass background - not a great background though
+#bio <- raster("/Volumes/data/data_repo/gis_data/Berner_2011_Kolyma_fire_biomass/kolyma_landsat5_larch_AGB_gm2_2007.tif")
+
+# read in all of the coordinates of ground control points
+sc <- read.csv("/Volumes/data/projects/vege_burn_siberia_2019/flight_info/gcp_coordinates.csv",
+               header = T)
+
+# select only those on fire perimeter boundaries
+# note there are duplicates, but I am being lazy
+b <- sc[which(sc$tr_loc==0),]
+
+# mapp using ggmap, which has ggplot2 syntax, I think
+ggmap(c) +
+  geom_point(data = b, aes(x = lon, y = lat, color = site), size = 3, position = "jitter") +
+  #geom_point(aes(x = 161.399713, y = 68.739907, color = "orange"), shape = 8, size = 5) +
+  #geom_label_repel(aes(x = 161.399713, y = 68.739907, label = "Northeast Science Station", vjust = "bottom", hjust = "left", color = "black")) +
+  theme(legend.position = "bottom", #specify theme aspects including font/text and legend location 
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        legend.title = element_blank(),
+        text = element_text(face = "plain", color = "black", size = 16)) +
+  scale_color_manual(values = c("black", "red", "blue", "yellow")) +
+  ggsn::scalebar(c,location = "bottomright", dist = 100, dist_unit ="km", transform = T)
+
+# not sure why the scale bar isn't working
+  annotation_scale(location = "br", width_hint = 0.25, text_size = 12, text_face = NULL, text_family = "serif", text_col = "black") 
+
+
+
+
--- viper_alaska_map.R
+++ viper_alaska_map.R
@@ -29,8 +29,9 @@ library(maptools)
 st <- st_read("/Volumes/GoogleDrive/My Drive/GIS_Data/ESRIDATA1/USA/STATES.SHP")
 
 # get only the Alaska file and omit extraneous attribute data
+#ak <- st_transform(st[51,-c(1,3:52)], CRS("+proj=lcc =lon_0=-150 +lat_0=65 +lat1=60 +lat_1=65 +lon_1=-155"))
 ak <- st[51,-c(1,3:52)]
-
+ak2 <- st_transform(ak,3857)
 #read viper point data
 site <- read.csv("/Volumes/GoogleDrive/My Drive/Documents/research/NSF_VIPER_2015-18/borehole_data/viper_stations_15_16/site_info.csv",
                  header = T)
@@ -39,15 +40,35 @@ site$Long <- -site$Long
 
 #convert to sf object for plotting
 sc <- st_as_sf(site, coords = c("Long", "Lat"),crs = 4326)
-# sc <- SpatialPointsDataFrame(site[,c(7,6)], 
-#                               proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"),
-#                               site)
+sc2 <- 
  
-
 # create the map
 ggplot() +
-  geom_sf(data = ak, fill = "gray75", color = "black",expand =F) +
-  geom_sf(data = sc, shape = 21, size = 2, color = as.numeric(site$Biome)) +
-  coord_sf(label_graticule = "SW")
+  geom_sf(data = ak, fill = "gray75", color = "black") +
+  #geom_point(data = site, aes(x = Long, y = Lat, color = Biome), shape = 16, position = "jitter") +
+  geom_point(data = site, aes(x = Long, y = Lat, color = Biome), shape = 16, size = 3, position = "jitter") +
+  theme(legend.position = "bottom", #specify theme aspects including font/text and legend location 
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        legend.title = element_blank(),
+        text = element_text(face = "plain", color = "black", size = 16)) +
+#        ylab("Latitude") +
   
+  scale_color_manual(values = c("black", "red")) + #manually set the colors
+  
+  # scale_color_manual(values = c("boreal" = "black", "tunrda" = "red"),guide = "legend",
+  #                    labels = c("Boreal", "Tundra")) +
+  
+  coord_sf(label_graticule = "SW", xlim = c(-177, -131)) #specify axis limits and graticule locations
+
+#  coord_sf(crs = "+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=65 +lon_0=-146 +x_0=1000000 +y_0=0 +datum=NAD83 +units=m +no_defs", 
+#           xlim = c(983000, 1071000), ylim = c(892000, 941000), 
+#           expand = FALSE, label_graticule = "SW")
+    
+# save map file
+ggsave("/Volumes/GoogleDrive/My Drive/Documents/research/manuscripts/mcculloch_roots/ak_map.pdf", 
+#       plot = fig_manuscript2, 
+       width = 8, height = 8, dpi = 600)
+
+
   
