ef63fe461a94128cb2bc539da67c1d7927e3ef46
Mike Loranty <mloranty@colgate.edu>
2020-05-19 15:48:28 -0400

    updated labeling for AK VIPER figure

--- flare_uas_2019_map.R
+++ flare_uas_2019_map.R
@@ -34,8 +34,7 @@ library(ggsn)
 # get a background map based roughly on the bounding box for the sampling points
 # this should be giving google satellite imagery, but it is not...
 c <- get_map(c(left = 160.2, bottom = 67.7, right = 162.7, top = 68.9),
-             maptype = "watercolor", zoom = 3, source = "osm", force = TRUE,
-             filename = "/Volumes/data/data_repo/gis_data/cherskiy_area_features/kolyma_terrain_base")
+             maptype = "terrain", zoom = 9, source = "osm", force = TRUE)
 
 # read in all of the coordinates of ground control points
 sc <- read.csv("/Volumes/data/projects/vege_burn_siberia_2019/flight_info/gcp_coordinates.csv",
@@ -47,15 +46,16 @@ b <- sc[which(sc$tr_loc==0),]
 
 # mapp using ggmap, which has ggplot2 syntax, I think
 ggmap(c) +
-  geom_point(data = b, aes(x = lon, y = lat, color = site), size = 3, position = "jitter") +
-  geom_point(aes(x = 161.399713, y = 68.739907), shape = 8, size = 5, , color = "orange", show.legend = F) +
+  geom_point(data = b, aes(x = lon, y = lat, color = site), shape = 17,size = 2, position = "jitter") +
+  geom_point(aes(x = 161.399713, y = 68.739907),  size = 3, color = "gray25", show.legend = F) +
+  geom_text(aes(x = 161.399713-0.25, y = 68.739907, label = "Cherskiy"), show.legend = F) +
   #geom_label_repel(aes(x = 161.399713, y = 68.739907, label = "Northeast Science Station", vjust = "bottom", hjust = "left", color = "black")) +
   theme(legend.position = "bottom", #specify theme aspects including font/text and legend location 
         axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         legend.title = element_blank(),
         text = element_text(face = "plain", color = "black", size = 16)) +
-  scale_color_manual(values = c("black", "red", "blue", "purple", "orange")) +
+  scale_color_manual(values = c("orange", "purple", "blue", "red")) +
   ggsn::scalebar(location = "bottomleft", dist = 10, dist_unit ="km", 
                  st.bottom = F,anchor = c(x = 160.3, y = 67.75),
                  transform = T, model = "WGS84",
@@ -63,8 +63,16 @@ ggmap(c) +
                  x.min = 160.2,x.max = 162.7,
                  y.min = 67.7, y.max = 68.9)
 
+ggsave("/Volumes/GoogleDrive/My Drive/Documents/research/manuscripts/siberia_uas/figure_1_sitemap.png", 
+       #       plot = fig_manuscript2, 
+       width = 8, height = 8, dpi = 600)
+
+
+### MAKE PNG or JPG for ANNA ### 4/30/20
+
+
 # not sure why the scale bar isn't working
-  annotation_scale(location = "br", width_hint = 0.25, text_size = 12, text_face = NULL, text_family = "serif", text_col = "black") 
+# annotation_scale(location = "br", width_hint = 0.25, text_size = 12, text_face = NULL, text_family = "serif", text_col = "black") 
 
 
 
--- viper_alaska_map.R
+++ viper_alaska_map.R
@@ -34,21 +34,26 @@ st <- st_read("/Volumes/GoogleDrive/My Drive/GIS_Data/ESRIDATA1/USA/STATES.SHP")
 #ak <- st_transform(st[51,-c(1,3:52)], CRS("+proj=lcc =lon_0=-150 +lat_0=65 +lat1=60 +lat_1=65 +lon_1=-155"))
 ak <- st[51,-c(1,3:52)]
 ak2 <- st_transform(ak,3857)
-#read viper point data
+
+# get osm background map for the region
+bg <- get_map(c(left = -170, bottom = 51, right = -135, top = 72),
+             maptype = "terrain", zoom = 7, source = "osm", force = TRUE)
+
+#read viper point data, omit west dock since this site was not sampled. 
 site <- read.csv("/Volumes/GoogleDrive/My Drive/Documents/research/NSF_VIPER_2015-18/borehole_data/viper_stations_15_16/site_info.csv",
-                 header = T)
+                 header = T)[-13,]
 # longitude values should be negative because we are in the western hemisphere
 site$Long <- -site$Long
 
 #convert to sf object for plotting
 sc <- st_as_sf(site, coords = c("Long", "Lat"),crs = 4326)
-sc2 <- 
  
 # create the map
 ggplot() +
   geom_sf(data = ak, fill = "gray75", color = "black") +
   #geom_point(data = site, aes(x = Long, y = Lat, color = Biome), shape = 16, position = "jitter") +
-  geom_point(data = site, aes(x = Long, y = Lat, color = Biome), shape = 16, size = 3, position = "jitter") +
+  geom_point(data = site, aes(x = Long, y = Lat, color = Biome), shape = 16, size = 2, position = "jitter") +
+  geom_text_repel(data = site, aes(x = Long, y = Lat, label = Site.Number), size = 2.5, position = "jitter") +
   theme(legend.position = "bottom", #specify theme aspects including font/text and legend location 
         axis.title.x = element_blank(),
         axis.title.y = element_blank(),
@@ -72,5 +77,20 @@ ggsave("/Volumes/GoogleDrive/My Drive/Documents/research/manuscripts/mcculloch_r
 #       plot = fig_manuscript2, 
        width = 8, height = 8, dpi = 600)
 
-
+## version using ggmap
+ggmap(bg) +
+  geom_point(data = site, aes(x = Long, y = Lat, color = Biome), shape = 20, size = 3, position = "jitter") +
+  theme(legend.position = "bottom", #specify theme aspects including font/text and legend location 
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        legend.title = element_blank(),
+        text = element_text(face = "plain", color = "black", size = 16)) +
+  #        ylab("Latitude") +
+#manually set the colors  
+  scale_color_manual(values = c("yellow", "blue")) 
+  # coord_sf(crs = "+proj=aea +lat_1=60 +lat_2=70 +lat_0=50 +lon_0=-176 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs", 
+  #          xlim = c(-100000, 1071000), ylim = c(0, 9410000), 
+  #          expand = FALSE, label_graticule = "SW")
+#specify axis limits and graticule locations
+  #coord_sf(label_graticule = "SW", xlim = c(-177, -131)) 
   
